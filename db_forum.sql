fe669b1897e8a2dee9eaf0ec4a9762a88ff18536
Gifsta <96801283+Gifsta@users.noreply.github.com>
2024-07-16 11:23:04 +0200

    init

---
+++ db_forum.sql
@@ -0,0 +1,59 @@
+CREATE TABLE `post`(
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `title` VARCHAR(100) NOT NULL,
+    `content` TEXT NOT NULL,
+    `id_user` BIGINT NOT NULL,
+    `like` BIGINT NOT NULL,
+    `id_categorie` BIGINT NOT NULL
+);
+CREATE TABLE `image`(
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `link` TEXT NOT NULL
+);
+CREATE TABLE `sous_categorie`(
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `id_categorie` BIGINT NOT NULL,
+    `name` VARCHAR(100) NOT NULL
+);
+CREATE TABLE `categorie`(
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(100) NOT NULL
+);
+CREATE TABLE `report`(
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `report_post` BIGINT NULL,
+    `report_comment` BIGINT NULL,
+    `id_user` BIGINT NOT NULL
+);
+CREATE TABLE `comment`(
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `title` VARCHAR(100) NOT NULL,
+    `content` TEXT NOT NULL,
+    `likes` BIGINT NOT NULL,
+    `id_post` BIGINT NOT NULL
+);
+CREATE TABLE `user`(
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `email` VARCHAR(150) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `pseudo` VARCHAR(100) NOT NULL,
+    `statut` VARCHAR(50) NOT NULL,
+    `pdp` INT NOT NULL,
+    `bio` TEXT NULL
+);
+ALTER TABLE
+    `user` ADD CONSTRAINT `user_pdp_foreign` FOREIGN KEY(`pdp`) REFERENCES `image`(`id`);
+ALTER TABLE
+    `report` ADD CONSTRAINT `report_id_user_foreign` FOREIGN KEY(`id_user`) REFERENCES `user`(`id`);
+ALTER TABLE
+    `post` ADD CONSTRAINT `post_id_categorie_foreign` FOREIGN KEY(`id_categorie`) REFERENCES `sous_categorie`(`id`);
+ALTER TABLE
+    `report` ADD CONSTRAINT `report_report_post_foreign` FOREIGN KEY(`report_post`) REFERENCES `post`(`id`);
+ALTER TABLE
+    `sous_categorie` ADD CONSTRAINT `sous_categorie_id_categorie_foreign` FOREIGN KEY(`id_categorie`) REFERENCES `categorie`(`id`);
+ALTER TABLE
+    `post` ADD CONSTRAINT `post_id_user_foreign` FOREIGN KEY(`id_user`) REFERENCES `user`(`id`);
+ALTER TABLE
+    `comment` ADD CONSTRAINT `comment_id_post_foreign` FOREIGN KEY(`id_post`) REFERENCES `post`(`id`);
+ALTER TABLE
+    `report` ADD CONSTRAINT `report_report_comment_foreign` FOREIGN KEY(`report_comment`) REFERENCES `comment`(`id`);
